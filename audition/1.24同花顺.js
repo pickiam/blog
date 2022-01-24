94db88f1b60e4092271c648884d28977389d689c
tmp <tmp@1deMacBook-Pro.lan>
2022-01-24 23:04:56 +0800

    feat: 1.24

---
+++ audition/1.24同花顺.js
@@ -0,0 +1,29 @@
+// 那个项目映象最深刻
+
+// '==' 和 '===' 区别
+
+// vue怎么不收集依赖  vue2.0 和 3.0的区别 数组是怎么收集依赖的
+
+// http1.1 http2的区别 https的基本流程
+
+// canvas生成的合成层和translate生成的合成层的区别 canvas context3d和context2d 的区别
+
+// 精度问题 前端怎么防止 tofix 产生的精度问题
+
+// 发起请求到页面渲染 经过的步骤
+
+//  class extends实现的继承 跟普通寄生组合继承的区别在哪 class super的作用 可以用到的地方
+
+// __proto 和 protoType的关系 Funtion.prototype和Object.prototype的关系
+
+// ts相关问题（好多）
+
+// 数据结构 数组和链表的区别 查询那个快 为什么
+
+// 虚拟列表的实现思路
+
+//vue动态路由的添加
+
+//用过的ui组件 用没有基于表单组件 进行二次封装
+
+
